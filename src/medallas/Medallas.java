fa6e479c2cada2ce94a56fe8c471fef371dfb31b
mauri <mauri@192.168.0.18>
2023-11-09 18:30:42 -0600

    Adding part 2

--- src/medallas/Medallas.java
+++ src/medallas/Medallas.java
@@ -5,74 +5,69 @@
 package medallas;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
-/**
- *
- * @author mauri
- */
 public class Medallas {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
+        Scanner sc = new Scanner(System.in);
 
-            // El numero 3 representa el tamaño de la lista, 
-            String[] nombresPaises = new String[3];
-            int[] numeroOro = new int[3];
-            int[] numeroPlata = new int[3];
-            int[] numeroBronce = new int[3];
-            int[] sumaPais = new int[3];
-            
-            // Ciclo for para recorrer la lista y pedir los datos, se le suma uno por que los arreglos empiezan en 0
-            for (int i = 0; i < 3; i++) {
-                System.out.print("\nNombre del país " + (i + 1) + ": \n");
-                nombresPaises[i] = sc.nextLine();
-                
-                System.out.print("\nNúmero de medallas de oro: \n");
-                numeroOro[i] = sc.nextInt();
-                
-                System.out.print("\nNúmero de medallas de plata: \n");
-                numeroPlata[i] = sc.nextInt();
-                
-                System.out.print("\nNúmero de medallas de bronce: \n");
-                numeroBronce[i] = sc.nextInt();
-                
-                sc.nextLine(); 
-            }
-            
-           // Calculo de los puntajes por pais
-           // Valor por Oro = 3; Valor por Plata = 2; Valor por Bronce = 1;
-            for (int i = 0; i < 3; i++) {
-                sumaPais[i] = (numeroOro[i]*3) + (numeroPlata[i]*2) + numeroBronce[i];
-            }
-            
-            // Calcular el país con mayor puntaje
-            int indiceMaxPuntaje = 0;
-            boolean empate = false;
+        System.out.print("Número de países del torneo: ");
+        int numPaises = sc.nextInt();
+        sc.nextLine(); 
 
-            for (int i = 1; i < 3; i++) {
-                if (sumaPais[i] > sumaPais[indiceMaxPuntaje]) {
-                    indiceMaxPuntaje = i;
-                    empate = false;
-                }
-                else if (sumaPais[i] == sumaPais[indiceMaxPuntaje]) {
-                    empate = true;
-                }
-            }
-            
-            // Imprimir los puntajes totales
-            for (int i = 0; i < 3; i++) {
-                System.out.println("\nEl puntaje total de " + nombresPaises[i] + " es: " + sumaPais[i] + "\n");
-            }
-            
-           // Pais con mejores puntos
-            if (empate) {
-                System.out.println("\nHay un empate\n");
-            } else {
-                System.out.println("\nEl país que obtuvo mayor puntaje es: " + nombresPaises[indiceMaxPuntaje]+ "\n");
+        String[] nombresPaises = new String[numPaises];
+        int[] numeroOro = new int[numPaises];
+        int[] numeroPlata = new int[numPaises];
+        int[] numeroBronce = new int[numPaises];
+        int[] sumaPais = new int[numPaises];
+
+        for (int i = 0; i < numPaises; i++) {
+            System.out.print("\nNombre del país " + (i + 1) + ": ");
+            nombresPaises[i] = sc.nextLine();
+
+            System.out.print("Número de medallas de oro: ");
+            numeroOro[i] = sc.nextInt();
+
+            System.out.print("Número de medallas de plata: ");
+            numeroPlata[i] = sc.nextInt();
+
+            System.out.print("Número de medallas de bronce: ");
+            numeroBronce[i] = sc.nextInt();
+
+            sc.nextLine(); 
+
+           
+            sumaPais[i] = (numeroOro[i] * 3) + (numeroPlata[i] * 2) + numeroBronce[i];
+            System.out.println("El puntaje total de " + nombresPaises[i] + " es: " + sumaPais[i]);
+        }
+
+       
+        int maxPuntaje = sumaPais[0];
+        ArrayList<Integer> indicesMaxPuntaje = new ArrayList<>();
+        indicesMaxPuntaje.add(0);
+
+        // Logica para determinar empates
+        for (int i = 1; i < sumaPais.length; i++) {
+            if (sumaPais[i] > maxPuntaje) {
+                maxPuntaje = sumaPais[i];
+                indicesMaxPuntaje.clear();
+                indicesMaxPuntaje.add(i);
+            } else if (sumaPais[i] == maxPuntaje) {
+                indicesMaxPuntaje.add(i);
             }
         }
+
+        
+        if (indicesMaxPuntaje.size() == 1) {
+            System.out.println("\nEl país con mayor puntaje total es: " + nombresPaises[indicesMaxPuntaje.get(0)]);
+        } else {
+            System.out.println("\nHay un empate para el mayor puntaje entre los siguientes países:");
+            for (int indice : indicesMaxPuntaje) {
+                System.out.println(nombresPaises[indice] + " con un puntaje de " + maxPuntaje);
+            }
+        }
+
+        sc.close();
     }
 }
